515d9cc77ea5c293694b2dd908a0c36f7c8825e9
Starseal <andzej.petkevic@mif.stud.vu.lt>
2014-10-18 18:11:45 +0300

    First C# assignment

---
+++ Date.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    struct Date
+    {
+        int day, month, year;
+        public bool valid{ get; private set; }
+
+
+        public Date(string input) : this()
+        {
+            string[] temp = input.Split('-');
+            
+            try
+            {
+                day = Convert.ToInt32(temp[0]);
+                month = Convert.ToInt32(temp[1]);
+                year = Convert.ToInt32(temp[2]);
+            }
+            catch (Exception e)
+            {
+                day = 0;
+                month = 0;
+                year = 0;
+            }
+            validate();
+            
+
+        }
+
+        private void validate()
+        {
+
+            if (day > 31 || day < 1 || month < 1 || month > 12)
+                valid = false;
+            else if (day > 28 && month == 2 && year % 4 != 0)
+                valid = false;
+
+            else if (day == 31)
+            {
+                switch (month)
+                {
+                    case 2:
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        valid = false;
+                        break;
+                    default:
+                        valid = true;
+                        break;
+                }
+            }
+            else
+                valid = true;
+       }
+
+        public override string ToString()
+        {
+            return day.ToString() + "-" + month.ToString() + "-" + year.ToString();
+        }
+    }
+}
---
+++ Event.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Calendar
+{
+    class Event
+    {
+        static Rijndael myRijndael =  Rijndael.Create();
+
+        public Date date;
+        private byte[] encrypted;
+
+        public string description
+        {
+            get
+            {
+                return DecryptStringFromBytes(encrypted, myRijndael.Key, myRijndael.IV);
+            }
+            set
+            {
+                encrypted = EncryptStringToBytes(value, myRijndael.Key, myRijndael.IV);
+            }
+        }
+        public Event (Date date, string description)
+        {
+            this.date = date;
+            this.description = description;
+
+        }
+        
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encryption;
+
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            swEncrypt.Write(plainText);
+                        }
+                        encryption = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return encryption;
+
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+ 
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+
+
+    }
+}
